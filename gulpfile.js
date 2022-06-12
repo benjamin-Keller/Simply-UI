f77d61df29f590afae580a16db406d414a58eb39
benjamin-Keller <zhero240@gmail.com>
2022-06-12 21:02:42 +0200

    Moves to base file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const { src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
-function buildStyles() {
-    return src('*.scss')
+buildStyles = () => {
+    return src('simply-ui/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
-function watchTask() {
-    watch(['*.scss'], buildStyles)
+watchTask = () => {
+    watch(['simply-ui/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
