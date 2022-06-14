312f33f3e03ccd89029b19b4d8406b85f781f168
benjamin-Keller <zhero240@gmail.com>
2022-06-14 21:57:57 +0200

    Adds gulp-purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 buildStyles = () => {
     return src('simply-ui/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 watchTask = () => {
-    watch(['simply-ui/**/*.scss'], buildStyles)
+    watch(['simply-ui/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
