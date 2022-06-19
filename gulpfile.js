883e671aa20b1b2769bf8984796f7cea6b38a15e
benjamin-Keller <zhero240@gmail.com>
2022-06-19 22:55:56 +0200

    Adds pipes for .min and .purge files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 const { src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss')
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 buildStyles = () => {
     return src('sass/**/*.scss')
         .pipe(sass())
-        // .pipe(purgecss({ content: ['*.html', '*/*.html', '*/**/*.html']}))
+        .pipe(rename('simply.css'))
         .pipe(dest('dist/css'))
+
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename('simply.min.css'))
+        .pipe(dest('dist/css'))
+
+        .pipe(sass())
+        .pipe(purgecss({ content: ['*.html', '*/*.html', '*/**/*.html']}))
+        .pipe(rename('simply.purged.css'))
+        .pipe(dest('dist/css'));
 }
 
 watchTask = () => {
